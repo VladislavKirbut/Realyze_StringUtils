b75e2249ec594d347a6a45f0fe750a0900f8e813
Vladislav_Global <vladalexkirbut2003@gmail.com>
2023-02-06 22:21:40 +0300

    Add function - changeFormatNumberOfCard

--- src/by/teachmeskills/kirbut/hw10/StringUtils.java
+++ src/by/teachmeskills/kirbut/hw10/StringUtils.java
@@ -2,7 +2,7 @@ package by.teachmeskills.kirbut.hw10;
 
 public class StringUtils {
 
-    private final static int SPACE = 32;
+    private static final int SPACE = 32;
 
     /*
     * str - string whose passed as parameter
@@ -23,4 +23,36 @@ public class StringUtils {
 
         return new String(str1, 0, count);
     }
+
+    /*
+    * str - string whose passed as a parameter
+    * lastNumbers - four last numbers of card
+    * count - counter of array elements
+    */
+    public static String changeFormatOfCard(String str) {
+        str = str.trim();
+
+        if (str.length() != 16)
+            throw new IllegalArgumentException("Enter correct number of card.");
+
+        for (int i = 0; i < str.length(); i++)
+            if (str.charAt(i) < '0' || str.charAt(i) > '9')
+                throw new IllegalArgumentException("Your number of card isn't correct. Please enter again.");
+
+        String lastNumbers = str.substring(str.length() - 4);
+
+        int count = 0;
+        char[] str1 = str.toCharArray();
+        for (int i = 1; i < str1.length; i++) {
+            if (i % 5 != 0)
+                str1[count++] = '*';
+            else str1[count++] = SPACE;
+        }
+
+        str = String.valueOf(str1).substring(0, 15);
+        str = str + lastNumbers;
+
+        return str;
+    }
+
 }
